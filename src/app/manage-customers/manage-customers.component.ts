9680a1cbf737422fa48d936b57921b1b73313e7a
LahiruSasanka2 <lahirusasanka2015@gmail.com>
2019-03-06 16:01:15 +0530

    Angular-POS-Routing-1.2.8

--- src/app/manage-customers/manage-customers.component.ts
+++ src/app/manage-customers/manage-customers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Customer} from '../dto/customer';
 import {HttpClient} from '@angular/common/http';
+import {el} from '@angular/platform-browser/testing/src/browser_util';
 
 @Component({
   selector: 'app-manage-customers',
@@ -14,12 +15,24 @@ export class ManageCustomersComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.loadAllCustomer();
+  }
+  loadAllCustomer():void{
     this.http.get<Customer[]>("http://localhost:8080/api/v1/customers").subscribe(customers1=>{
-     this.customers = customers1;
+      this.customers = customers1;
     });
   }
+
   deleteCustomer(customer: Customer): void {
-    this.http.delete("http://localhost:8080/api/v1/customers/" + customer.id);
+    this.http.delete("http://localhost:8080/api/v1/customers/" + customer.id,{observe:'response'}).subscribe(
+      response => {
+        if (response.status == 200){
+          this.loadAllCustomer();
+        } else {
+          alert("Faild to Customer the Delete");
+        }
+      }
+    );
   }
 
 }
