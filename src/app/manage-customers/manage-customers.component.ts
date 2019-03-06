e22844fd2b14f3707d42463b546f5ddf5edcbe9d
LahiruSasanka2 <lahirusasanka2015@gmail.com>
2019-03-06 15:47:00 +0530

    Angular-POS-Routing-1.2.4

--- src/app/manage-customers/manage-customers.component.ts
+++ src/app/manage-customers/manage-customers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Customer} from '../dto/customer';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-customers',
@@ -10,9 +11,12 @@ export class ManageCustomersComponent implements OnInit {
 
   customers: Customer[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<Customer[]>("http://localhost:8080/api/v1/customers").subscribe(customers1=>{
+     this.customers = customers1;
+    });
   }
 
 }
