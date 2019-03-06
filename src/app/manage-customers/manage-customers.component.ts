ba931ebbc427a1ad1cb1ebff9079625c41f0d95e
LahiruSasanka2 <lahirusasanka2015@gmail.com>
2019-03-06 15:36:58 +0530

    Angular-POS-Routing-1.2.2

--- src/app/manage-customers/manage-customers.component.ts
+++ src/app/manage-customers/manage-customers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from '../dto/customer';
 
 @Component({
   selector: 'app-manage-customers',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-customers.component.css']
 })
 export class ManageCustomersComponent implements OnInit {
+
+  customers: Customer[] = [];
 
   constructor() { }
 
