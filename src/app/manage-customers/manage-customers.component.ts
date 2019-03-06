566222e6e432a6ac58d6c9c46fa689823d9549ca
LahiruSasanka2 <lahirusasanka2015@gmail.com>
2019-03-06 15:55:35 +0530

    Angular-POS-Routing-1.2.7

--- src/app/manage-customers/manage-customers.component.ts
+++ src/app/manage-customers/manage-customers.component.ts
@@ -19,7 +19,7 @@ export class ManageCustomersComponent implements OnInit {
     });
   }
   deleteCustomer(customer: Customer): void {
-    
+    this.http.delete("http://localhost:8080/api/v1/customers/" + customer.id);
   }
 
 }
